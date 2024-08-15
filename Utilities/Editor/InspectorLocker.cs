67a2d5c411e81960cb5d4dc867adecedfd50e16b
Jimothy <antijulius@gmail.com>
2024-08-15 00:45:51 +0000

    Tidy

--- Utilities/Editor/InspectorLocker.cs
+++ Utilities/Editor/InspectorLocker.cs
@@ -6,18 +6,6 @@ namespace Jimothy.Utilities.Editor
 {
     public static class InspectorLocker
     {
-        private static readonly MethodInfo FlipLocked;
-        private const BindingFlags Flags = BindingFlags.NonPublic | BindingFlags.Instance;
-
-        static InspectorLocker()
-        {
-            // Cache static MethodInfo and PropertyInfo for performance
-            var editorLockTrackerType =
-                typeof(EditorGUIUtility).Assembly.GetType(
-                    "UnityEditor.EditorGUIUtility+EditorLockTracker");
-            FlipLocked = editorLockTrackerType.GetMethod("FlipLocked", Flags);
-        }
-        
         [MenuItem("Edit/Lock Inspector %l")]
         public static void ToggleLock()
         {
