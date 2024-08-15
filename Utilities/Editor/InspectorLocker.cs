7c0f7a6ecdb9fefd55cc15b39845946404e4b0b6
Jimothy <antijulius@gmail.com>
2024-08-15 00:40:12 +0000

    Add inspector locking shortcut

---
+++ Utilities/Editor/InspectorLocker.cs
@@ -0,0 +1,38 @@
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+
+namespace Jimothy.Utilities.Editor
+{
+    public static class InspectorLocker
+    {
+        private static readonly MethodInfo FlipLocked;
+        private static readonly PropertyInfo ConstrainProportions;
+        private const BindingFlags Flags = BindingFlags.NonPublic | BindingFlags.Instance;
+
+        static InspectorLocker()
+        {
+            // Cache static MethodInfo and PropertyInfo for performance
+            var editorLockTrackerType =
+                typeof(EditorGUIUtility).Assembly.GetType(
+                    "UnityEditor.EditorGUIUtility+EditorLockTracker");
+            FlipLocked = editorLockTrackerType.GetMethod("FlipLocked", Flags);
+
+            ConstrainProportions = typeof(Transform).GetProperty("constrainProportions", Flags);
+        }
+        
+        [MenuItem("Edit/Lock Inspector %l")]
+        public static void ToggleLock()
+        {
+            ActiveEditorTracker.sharedTracker.isLocked =
+                !ActiveEditorTracker.sharedTracker.isLocked;
+            ActiveEditorTracker.sharedTracker.ForceRebuild();
+        }
+
+        [MenuItem("Edit/Lock Inspector %l", true)]
+        public static bool IsValid()
+        {
+            return ActiveEditorTracker.sharedTracker.activeEditors.Length > 0;
+        }
+    }
+}
