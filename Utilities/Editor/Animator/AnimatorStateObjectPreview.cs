b0bc0f996db7436107dd8f3038677d9474e624df
Jimothy <iojimothy@gmail.com>
2025-06-05 21:45:53 +0000

    Fix animation preview length

--- Utilities/Editor/Animator/AnimatorStateObjectPreview.cs
+++ Utilities/Editor/Animator/AnimatorStateObjectPreview.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine;
@@ -7,6 +8,10 @@ namespace Jimothy.Utilities.Editor.Animator
     [CustomPreview(typeof(AnimatorState))]
     public class AnimatorStateObjectPreview : ObjectPreview
     {
+        private static FieldInfo _cachedAvatarPreviewField;
+        private static FieldInfo _cachedTimeControlField;
+        private static FieldInfo _cachedStopTimeField;
+
         private UnityEditor.Editor _preview;
         private int _animationClipId;
 
@@ -16,12 +21,29 @@ namespace Jimothy.Utilities.Editor.Animator
 
             if (targets.Length > 1 || Application.isPlaying) return;
 
+            SourceAnimationClipEditorFields();
+
             AnimationClip clip = GetAnimationClip(target as AnimatorState);
             if (clip != null)
             {
+                AnimationClipEditor editor = UnityEditor.Editor.CreateEditor(clip) as AnimationClipEditor;
                 _preview = UnityEditor.Editor.CreateEditor(clip);
                 _animationClipId = clip.GetInstanceID();
             }
+        }
+
+        private void SourceAnimationClipEditorFields()
+        {
+            if (_cachedAvatarPreviewField != null) return;
+
+            // Reflection can get messy between Unity versions.
+            // If you run into issues, you may need to adjust the field names.
+            _cachedAvatarPreviewField = System.Type.GetType("UnityEditor.AnimationClipEditor, UnityEditor")
+                .GetField("m_AvatarPreview", BindingFlags.NonPublic | BindingFlags.Instance);
+            _cachedTimeControlField = System.Type.GetType("UnityEditor.AvatarPreview, UnityEditor")
+                .GetField("timeControl", BindingFlags.Public | BindingFlags.Instance);
+            _cachedStopTimeField = System.Type.GetType("UnityEditor.TimeControl, UnityEditor")
+                .GetField("stopTime", BindingFlags.Public | BindingFlags.Instance);
         }
 
         public override bool HasPreviewGUI()
@@ -44,8 +66,20 @@ namespace Jimothy.Utilities.Editor.Animator
 
             if (_preview != null)
             {
+                UpdateAnimationClipEditor(_preview, currentClip);
                 _preview.OnInteractivePreviewGUI(r, background);
             }
+        }
+
+        private void UpdateAnimationClipEditor(UnityEditor.Editor editor, AnimationClip currentClip)
+        {
+            if (_cachedAvatarPreviewField == null || _cachedTimeControlField == null || _cachedStopTimeField == null)
+                return;
+            
+            var avatarPreview = _cachedAvatarPreviewField.GetValue(editor);
+            var timeControl = _cachedTimeControlField.GetValue(avatarPreview);
+            
+            _cachedStopTimeField.SetValue(timeControl, currentClip.length);
         }
 
         public override void Cleanup()
